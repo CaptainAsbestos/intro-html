a3b37adbc3605ba6c05b9b69864d64fd43b66ce7
Captain Asbestos <71844282+CaptainAsbestos@users.noreply.github.com>
2023-11-03 13:45:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-var date = new Date();
-var hour = date.getHours();
-var min = date.getMinutes();
+//var date = new Date();
+//var hour = date.getHours();
+//var min = date.getMinutes();
 //document.write(hour.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping:false}) + ":" + min.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping:false}));
 //console.log("Done!");
 
+function updateClock() {
+  var date = new Date();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  
   var timeString = hour.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) + ":" + min.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-
- // Update the clock display
-  document.getElementById("clock").textContent = timeString;
+  
+  // Clear the document and write the updated time
+  document.open();
+  document.write(timeString);
+  document.close();
 }
 
 // Initial call to update the clock
