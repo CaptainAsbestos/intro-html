90f58d43b07181a8f5ee709fb5d2de186379f081
Captain Asbestos <71844282+CaptainAsbestos@users.noreply.github.com>
2023-11-03 13:30:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,59 @@
-var span = document.getElementById('span');
+function Time() {
 
-function time() {
-  var d = new Date();
-  var s = d.getSeconds();
-  var m = d.getMinutes();
-  var h = d.getHours();
-  span.textContent = 
-    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+ // Creating object of the Date class
+ var date = new Date();
+
+ // Get current hour
+ var hour = date.getHours();
+ // Get current minute
+ var minute = date.getMinutes();
+ // Get current second
+ var second = date.getSeconds();
+
+ // Variable to store AM / PM
+ var period = "";
+
+ // Assigning AM / PM according to the current hour
+ if (hour >= 12) {
+ period = "PM";
+ } else {
+ period = "AM";
+ }
+
+ // Converting the hour in 12-hour format
+ if (hour == 0) {
+ hour = 12;
+ } else {
+ if (hour > 12) {
+ hour = hour - 12;
+ }
+ }
+
+ // Updating hour, minute, and second
+ // if they are less than 10
+ hour = update(hour);
+ minute = update(minute);
+ second = update(second);
+
+ // Adding time elements to the div
+ document.getElementById("digital-clock").innerText = hour + " : " + minute + " : " + second + " " + period;
+
+ // Set Timer to 1 sec (1000 ms)
+ setTimeout(Time, 1000);
 }
 
-setInterval(time, 1000);
+ // Function to update time elements if they are less than 10
+ // Append 0 before time elements if they are less than 10
+function update(t) {
+ if (t < 10) {
+ return "0" + t;
+ }
+ else {
+ return t;
+ }
+}
 
+Time();
 
 //var date = new Date();
 //var hour = date.getHours();
