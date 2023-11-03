356a26f3e4f6fd4ce98112b1f54a1f5f2b6c5353
Captain Asbestos <71844282+CaptainAsbestos@users.noreply.github.com>
2023-11-03 13:22:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-var date = new Date();
-var hour = date.getHours();
-var min = date.getMinutes();
-document.write(hour.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping:false}) + ":" + min.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping:false}));
-console.log("Done!");
+var span = document.getElementById('span');
+
+function time() {
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  span.textContent = 
+    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+}
+
+setInterval(time, 1000);
+
+
+//var date = new Date();
+//var hour = date.getHours();
+//var min = date.getMinutes();
+//document.write(hour.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping:false}) + ":" + min.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping:false}));
+//console.log("Done!");
